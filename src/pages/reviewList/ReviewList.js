1da0423a6466dfb24de09c6ce7f6f7049cb25cdf
IvanYou98 <yuanfanyou@gmail.com>
2022-06-19 21:22:19 -0400

    fix the date format

--- src/pages/reviewList/ReviewList.js
+++ src/pages/reviewList/ReviewList.js
@@ -8,33 +8,49 @@ import {db} from "../../firebase"
 import {AuthContext} from "../../context/AuthContext";
 import {useNavigate} from "react-router-dom";
 
+
 const columns = [
     {
         field: 'title',
         headerName: 'Title',
-        width: 150,
-        editable: true,
+        width: 250,
+        renderCell: cell =>
+            <h4>{cell.row.title}</h4>
+        ,
+        editable: false
     },
     {
         field: 'problemLink',
         headerName: 'Link to Problem',
+        renderCell: cell =>
+            <a href={cell.row.problemLink}>{cell.row.problemLink}</a>,
         width: 450,
         type: 'string',
-        editable: true,
     },
     {
         field: 'lastCompleteDate',
         headerName: 'Last Complete Date',
         type: 'date',
         width: 210,
-        editable: true,
     },
     {
         field: 'currentRound',
         headerName: 'Round',
         type: 'string',
-        width: 100,
-        editable: true
+    },
+    {
+        field: 'edit',
+        headerName: 'Edit',
+        renderCell: cell =>
+            <Button variant="contained" href={"/edit?problemId=" + cell.row.id} style={{height: '25px'}}>Edit</Button>
+
+    },
+    {
+        field: 'dummy',
+        headerName: 'Edit',
+        renderCell: cell =>
+            <div>{console.log(cell.row)}</div>
+
     }
 ];
 
@@ -45,27 +61,29 @@ const ReviewList = () => {
     const handleCreate = e => {
         navigate('/create');
     }
-    useEffect( () => {
-        const fetchData =  async () => {
+    useEffect(() => {
+        const fetchData = async () => {
             const uid = currentUser.uid;
             const querySnapshot = await getDocs(collection(db, uid));
             let list = [];
             querySnapshot.forEach((doc) => {
-                list.push(doc.data());
+                let problem = doc.data();
+                problem.id = doc.id;
+                problem.lastCompleteDate = new Date(problem.lastCompleteDate.seconds * 1000);
+                list.push(problem);
             })
             setData(list);
-            console.log(data);
         };
         fetchData();
     }, [])
     return (
-        <div style={{ height: 700, width: '80%', marginTop: '50px'}}>
+        <div style={{height: 700, width: '80%', marginTop: '50px'}}>
             <div className="createBtn">
-                <Selector style={{float: 'left'}} />
-                <Button variant="contained" style={{float: 'right', marginTop: "10px"}} onClick={handleCreate}>Create</Button>
+                <Selector style={{float: 'left'}}/>
+                <Button variant="contained" style={{float: 'right', marginTop: "10px"}}
+                        onClick={handleCreate}>Create</Button>
             </div>
             <DataGrid
-                getRowId={(r) => r.problemLink}
                 rows={data}
                 columns={columns}
                 checkboxSelection
