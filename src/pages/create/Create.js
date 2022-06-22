00cc8bb6088f9a80599c254e9209e6c6d459c977
IvanYou98 <yuanfanyou@gmail.com>
2022-06-21 20:09:00 -0400

    change placeholder of problemLink

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -22,7 +22,7 @@ const Create = () => {
             id: "problemLink",
             label: "Problem Link",
             type: "text",
-            placeholder: "www.leetcode.com/twoSum",
+            placeholder: "https://www.leetcode.com/twoSum",
         }
     ];
 
